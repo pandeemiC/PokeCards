3f7810e351f74aac0114e6e8447a7b7d19740467
Eren Sahin <lkooxer55@hotmail.com>
2024-06-08 17:59:59 +0300

    something

---
+++ src/util/PokeApi.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+    const fetchPokemon = async () => {
+        const capitalFirstLetter = (string) => {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        };
+        const statsToShow = ['attack', 'speed', 'special-attack', 'defense'];
+
+        try {
+            const randomPokemonId = Math.floor(Math.random() * 898) + 1;
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`);
+            if(!response.ok) {
+                throw new Error('Response was not ok.')
+            }
+            const data = await response.json();
+            
+             return {
+                name: data.name,
+                type: data.types.map(typeInfo => typeInfo.type.name).join(', '),
+                hp: data.stats.find(stat => stat.stat.name === 'hp').base_stat,
+                abilities: data.abilities.map(abilityInfo => capitalFirstLetter(abilityInfo.ability.name)).join(' | '),
+                stats: data.stats
+                .filter(statInfo => statsToShow.includes(statInfo.stat.name))
+                .map(statInfo => ({
+                    name: statInfo.stat.name,
+                    value: statInfo.base_stat,
+                })),
+                sprite: data.sprites.other.dream_world.front_default ? data.sprites.other.dream_world.front_default : data.sprites.other['official-artwork'].front_default,
+             };
+            
+        } catch (err) {
+            console.error('Error fetching Pokemon', err);
+            return null;
+        }
+    }
+
+    export default fetchPokemon;
+
---
+++ tailwind.config.js
@@ -0,0 +1,70 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  safelist: [
+    'bg-normal', 'bg-poison', 'bg-electric', 'bg-water', 'bg-grass', 'bg-ice',
+    'bg-ghost', 'bg-dark', 'bg-fighting', 'bg-ground', 'bg-flying', 'bg-psychic',
+    'bg-bug', 'bg-rock', 'bg-dragon', 'bg-steel', 'bg-fairy', 'bg-fire'
+  ],
+
+  theme: {
+    container: {
+      center: true,
+    },
+
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      animation: {
+        'animate-bounce': 'bounce 4s infinite',
+      },
+      backgroundImage: {
+        "poison": "url('/public/assets/poison-gradient.svg')",
+        "electric": "url('/public/assets/electric-gradient.svg')",
+        "normal": "url('/public/assets/normal-gradient.svg')",
+        "water": "url('/public/assets/water-gradient.svg')",
+        "grass": "url('/public/assets/grass-gradient.svg')",
+        "ice": "url('/public/assets/ice-gradient.svg')",
+        "ghost": "url('/public/assets/ghost-gradient.svg')",
+        "dark": "url('/public/assets/dark-gradient.svg')",
+        "fighting": "url('/public/assets/fighting-gradient.svg')",
+        "ground": "url('/public/assets/ground-gradient.svg')",
+        "flying": "url('/public/assets/flying-gradient.svg')",
+        "psychic": "url('/public/assets/psychic-gradient.svg')",
+        "bug": "url('/public/assets/bug-gradient.svg')",
+        "rock": "url('/public/assets/rock-gradient.svg')",
+        "dragon": "url('/public/assets/dragon-gradient.svg')",
+        "steel": "url('/public/assets/steel-gradient.svg')",
+        "fairy": "url('/public/assets/fairy-gradient.svg')",
+        "fire": "url('/public/assets/fire-gradient.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
+// colors: {
+//   "poison": "#c084fc",
+//   "type-electric": "#fcd34d",
+//   "type-normal": "#d4d4d8",
+//   "type-water": "#0ea5e9",
+//   "type-grass": "#4ade80",
+//   "type-ice": "#67e8f9",
+//   "type-ghost": "#4338ca",
+//   "type-dark": "#44403c",
+//   "type-fighting": "#ef4444",
+//   "type-ground": "#a16207",
+//   "type-flying": "#c7d2fe",
+//   "type-psychic": "#be185d",
+//   "type-bug": "#16a34a",
+//   "type-rock": "#21201e",
+//   "type-dragon": "#ea580c",
+//   "type-steel": "#78716c",
+//   "type-fairy": "#fda4af",
+// },
